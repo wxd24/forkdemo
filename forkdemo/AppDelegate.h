1f65dff80b62cd086a767ebff70267fa6cfe69b3
wxd24 <csiivip@163.com>
2019-04-24 10:50:43 +0800

    Initial Commit

---
+++ forkdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  forkdemo
+//
+//  Created by wangxiaodong on 24/04/2019.
+//  Copyright © 2019 晓东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
